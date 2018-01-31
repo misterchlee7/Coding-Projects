724ed9381f748bd14db09eb7554e9cd2c8f75d4f
Charlie Lee <misterchlee7@gmail.com>
2018-01-30 19:42:43 -0800

    fixed Students and Instructors script.js to be more efficient

--- WebFundamentals/Javascript/Students and Instructors/script.js
+++ WebFundamentals/Javascript/Students and Instructors/script.js
@@ -1,29 +1,21 @@
-var users = {
-  'Students': [
-    { first_name: 'Michael', last_name: 'Jordan' },
-    { first_name: 'John', last_name: 'Rosales' },
-    { first_name: 'Mark', last_name: 'Guillen' },
-    { first_name: 'KB', last_name: 'Tonel' }
-  ],
-  'Instructors': [
-    { first_name: 'Michael', last_name: 'Choi' },
-    { first_name: 'Martin', last_name: 'Puryear' }
-  ]
-}
+function naming(){
+  var users = {
+   'Students': [ 
+     {first_name:  'Michael', last_name : 'Jordan'},
+     {first_name : 'John', last_name : 'Rosales'},
+     {first_name : 'Mark', last_name : 'Guillen'},
+     {first_name : 'KB', last_name : 'Tonel'}
+    ],
+   'Instructors': [
+     {first_name : 'Michael', last_name : 'Choi'},
+     {first_name : 'Martin', last_name : 'Puryear'}
+    ]
+   }
 
-function naming(users) {
-
-  console.log("Students");
-
-  for (var key in users.Students) {
-    var i = (Number(key) + 1);
-    console.log(i + " - " + users.Students[key].first_name + " " + users.Students[key].last_name + " - " + (users.Students[key].first_name.length + users.Students[key].last_name.length));
-  }
-
-  console.log("Instructors");
-
-  for (key in users.Instructors) {
-    i = (Number(key) + 1);
-    console.log(i + " - " + users.Instructors[key].first_name + " " + users.Instructors[key].last_name + " - " + (users.Instructors[key].first_name.length + users.Instructors[key].last_name.length));
+  for(var key in users){
+    console.log(key);
+    for(var i = 0; i < users[key].length; i++){
+      console.log((i + 1) + " - " + users[key][i].first_name + " " + users[key][i].last_name + " - " + (users[key][i].first_name.length + users[key][i].last_name.length));
+    }
   }
 }
